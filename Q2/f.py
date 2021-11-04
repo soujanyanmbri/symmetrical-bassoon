b3ea2b3050a33d331235745dc6ca6f96b66a65a7
soujanyanmbri <namburisoujanya@gmail.com>
2021-11-04 16:40:51 +0530

    added f

---
+++ Q2/f.py
@@ -0,0 +1,72 @@
+#Classify the points (1,2,1)t,, (5,3,2)t
+#, (0,0,0)t
+#, (1,0,0)t using each feature vector
+#mentioned above and compare the results.
+
+#Use the covariance and mean of our own matrix
+import numpy as np
+from math import log, pi
+
+def discriminant(x, mean, covariance, dimension, probability):
+    #Check if it is univariate:numpy.linalg.LinAlgError: 1-dimensional array given. Array must be at least two-dimensional
+    if dimension == 1: 
+        dis = (-0.5*(x - mean) * (1 / covariance))* (x-mean) - 0.5*log(2*pi) - 0.5*log(covariance) 
+    else: 
+        temp =np.matmul(-0.5*(x - mean), np.linalg.inv(covariance))
+        dis = np.matmul(temp, (x-mean).T) -0.5*dimension*log(2*pi) - 0.5*log(np.linalg.det(covariance))
+    if(probability == 0):
+        return dis
+    else: 
+        dis += log(probability)
+    return dis
+
+def main():
+    # Given Data
+    dataclass = [
+            [[-5.01, -8.12, -3.68], [-5.43, -3.48, -3.54], [1.08, -5.52, 1.66], [0.86, -3.78, -4.11], [-2.67, 0.63, 7.39], [4.94, 3.29, 2.08], [-2.51, 2.09, -2.59], [-2.25, -2.13, -6.94], [5.56, 2.86, -2.26], [1.03, -3.33, 4.33]],
+            [[-0.91, -0.18, -0.05], [1.30, -2.06, -3.53], [-7.75, -4.54, -0.95], [-5.47, 0.50, 3.92], [6.14, 5.72, -4.85], [3.60, 1.26, 4.36], [5.37, -4.63, -3.65], [7.18, 1.46, -6.66], [-7.39, 1.17, 6.30], [-7.50, -6.32, -0.31]],
+            [[5.35, 2.26, 8.13], [5.12, 3.22, -2.66], [-1.34, -5.31, -9.87], [4.48, 3.42, 5.19], [7.11, 2.39, 9.21],[7.17, 4.33, -0.98], [5.75, 3.97, 6.65], [0.77, 0.27, 2.41], [0.90, -0.43, -8.71], [3.52, -0.36, 6.43]] 
+        ]
+    ix = [[1, 2, 1], [5, 3, 2], [0, 0, 0], [1, 0, 0]]
+
+    dataclasses = [np.array(l) for l in dataclass]    
+    #Find other values required for computing the values: 
+    n = len(dataclasses)            # number of classes 
+    
+    #Assuming each class is equally probable
+    probability = [0.5, 0.5, 0]
+
+    #Find mean and covariance    
+    means = []                      # d-component mean vector
+    covariance = []                 # d by d covariance matrix for each set
+    
+    #Finding means in each column
+    for sing in dataclasses:
+        means.append(sing.mean(axis=0))
+        covariance.append(np.cov(sing.T))
+    g = [0] * n
+    #taking the first feature vector: 
+    for ip in ix:
+        print("Case 1: Considering 1 feature: ") 
+        d = 1
+        for i in range(n):
+            g[i] = discriminant(ip[0], means[i][0], covariance[i][0][0], d, probability[i])
+        maximum_indices = g.index(max(g)) + 1
+        print(ip, "\t classified as: \t", maximum_indices )
+        print("Case 1: Considering 2 features: ") 
+        d = 2
+        for i in range(n):
+            g[i] = discriminant(ip[0:2], means[i][0:2], covariance[i][0:2, 0:2], d, probability[i])
+        maximum_indices = g.index(max(g)) + 1
+        print(ip, "\t classified as: \t", maximum_indices )
+        print("Case 1: Considering 3 features: ") 
+        d = 3
+        for i in range(n):
+            g[i] = discriminant(ip, means[i], covariance[i], d, probability[i])
+        maximum_indices = g.index(max(g)) + 1
+        print(ip, "\t classified as: \t", maximum_indices )
+            
+
+if __name__ == '__main__':
+    main()
+    
