4a708167ce333d6f0478185289297c79cf59eb1a
soujanyanmbri <namburisoujanya@gmail.com>
2021-11-04 13:25:32 +0530

    second one - a and b done

--- Q2/main.py
+++ Q2/main.py
@@ -6,7 +6,7 @@ from math import log, pi
 def discriminant(x, mean, covariance, dimension, probability):
     #Check if it is univariate:numpy.linalg.LinAlgError: 1-dimensional array given. Array must be at least two-dimensional
     if dimension == 1: 
-        dis = ((-0.5*(x - mean) * (1 / covariance))* (x-mean)) - 0.5*log(2*pi) - 0.5*log(covariance) 
+        dis =  ((-0.5*(x - mean) * (1 / covariance))* (x-mean)) - 0.5*log(2*pi) - 0.5*log(covariance) 
     else: 
         temp =np.matmul(-0.5*(x - mean), np.linalg.inv(covariance))
         dis = np.matmul(temp, (x-mean).T) -0.5*dimension*log(2*pi) - 0.5*log(np.linalg.det(covariance))
@@ -16,10 +16,6 @@ def discriminant(x, mean, covariance, dimension, probability):
     else: 
         dis += log(probability)
     return dis
-
-
-
-
 def main():
     # Given Data
     dataclasses = [
@@ -27,10 +23,11 @@ def main():
             np.array([[-0.91, -0.18, -0.05], [1.30, -2.06, -3.53], [-7.75, -4.54, -0.95], [-5.47, 0.50, 3.92], [6.14, 5.72, -4.85], [3.60, 1.26, 4.36], [5.37, -4.63, -3.65], [7.18, 1.46, -6.66], [-7.39, 1.17, 6.30], [-7.50, -6.32, -0.31]]),
             np.array([[5.35, 2.26, 8.13], [5.12, 3.22, -2.66], [-1.34, -5.31, -9.87], [4.48, 3.42, 5.19], [7.11, 2.39, 9.21],[7.17, 4.33, -0.98], [5.75, 3.97, 6.65], [0.77, 0.27, 2.41], [0.90, -0.43, -8.71], [3.52, -0.36, 6.43]]) 
         ]
-        
+
+    print (dataclasses)   
     #Find other values required for computing the values: 
     n = len(dataclasses)-1            # number of classes 
-    d = len(dataclasses[0][0])        # number of features
+    d = 1                           # number of features
 
     #Assuming each class is equally probable
     probability = [0.5, 0.5, 0]
@@ -43,16 +40,15 @@ def main():
     for sing in dataclasses:
         means.append(sing.mean(axis=0))
         covariance.append(np.cov(sing.T))
+  #  print(covariance)
     k = 1
     for dataclass in dataclasses:
-        if(k == 3):
-            continue
         print("The following data should be classified as: ", k, ", but are classified as: ")
         k+=1
         for data in dataclass:
             gi = [0] * n           # each gi 
             for i in range(n):
-                gi[i] = discriminant(data, means[i], covariance[i], d, probability[i])
+                gi[i] = discriminant(data[0], means[i][0], covariance[i][0][0], d, probability[i])
             #find maximum g[i]
             maximum_indices = gi.index(max(gi))
             print(data, "\t classified as: \t", maximum_indices + 1)
